3c956d0a5edba1ff578c1a4d25fa7153662899a3
Nicolas Balduzzi <nbalduzzi@zemsania.com>
2018-05-18 00:04:48 +0200

    test

--- __tests__/app.js
+++ __tests__/app.js
@@ -4,14 +4,14 @@ const path = require('path');
 const assert = require('yeoman-assert');
 const helpers = require('yeoman-test');
 
-describe('generator-fastify:app', () => {
-  beforeAll(() => {
+describe('generator-fastify:app', function() {
+  beforeAll(function() {
     return helpers
       .run(path.join(__dirname, '../generators/app'))
       .withPrompts({ projectName: 'fastify-test' });
   });
 
-  it('creates files', () => {
+  it('creates files', function() {
     assert.file(['src/app.js', 'src/routes.js', 'src/config/config.js']);
   });
 });
