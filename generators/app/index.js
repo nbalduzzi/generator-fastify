8b036e6d1251f9138c18ee15519f0e0a92dbc508
nbalduzzi <nicolas.balduzzi@agoraimages.com>
2021-02-09 12:15:30 +0100

    prettier

--- generators/app/index.js
+++ generators/app/index.js
@@ -34,7 +34,7 @@ module.exports = class extends Generator {
       }
     ];
 
-    return this.prompt(prompts).then(props => {
+    return this.prompt(prompts).then((props) => {
       // To access props later use this.props.someAnswer;
       this.props = props;
     });
