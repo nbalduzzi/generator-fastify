c4c7d27c22b941e9a8381567642ee64df10b1952
nbalduzzi <nicolas.balduzzi@agoraimages.com>
2021-02-10 09:57:30 +0100

    feat(version): bumpversion

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # generator-fastify
 
+![Test](https://github.com/nbalduzzi/generator-fastify/workflows/Test/badge.svg)
+![Semantic Release](https://github.com/nbalduzzi/generator-fastify/workflows/Semantic%20Release/badge.svg)
 [![semantic-release](https://img.shields.io/badge/%20%20%F0%9F%93%A6%F0%9F%9A%80-semantic--release-e10079.svg)](https://github.com/semantic-release/semantic-release)
 
 [![NPM](https://nodei.co/npm/generator-fastify.png?downloads=true&downloadRank=true&stars=true)](https://nodei.co/npm/generator-fastify/)
--- generators/app/templates/fastify-archetype/src/app.js
+++ generators/app/templates/fastify-archetype/src/app.js
@@ -1,27 +1,28 @@
-const swagger = require('fastify-swagger')
-const cors = require('cors')
+const swagger = require('fastify-swagger');
+const cors = require('cors');
 
-const { name, port, options, documentation } = require('./config/config')
-const { v1 } = require('./routes')
+const { name, port, options, documentation } = require('./config/config');
+const { v1 } = require('./routes');
 
-const fastify = require('fastify')(options)
+const fastify = require('fastify')(options);
 
 // Middlewares
-fastify.use(cors())
+fastify.use(cors());
+
 // Plugins
-fastify.register(swagger, documentation)
-fastify.register(v1, { prefix: '/v1' })
+fastify.register(swagger, documentation);
+fastify.register(v1, { prefix: '/v1' });
 
 // Server
-fastify.listen(port, '0.0.0.0', err => {
+fastify.listen(port, '0.0.0.0', (err) => {
   if (err) {
-    fastify.log.error(err)
-    process.exit(1)
+    fastify.log.error(err);
+    process.exit(1);
   }
 
-  fastify.swagger()
-  fastify.log.info('%s listening in %s environment', name, process.env.NODE_ENV)
-})
+  fastify.swagger();
+  fastify.log.info('%s listening in %s environment', name, process.env.NODE_ENV);
+});
 
 // Export fastify for testing purpose
-module.exports = { fastify }
+module.exports = { fastify };
